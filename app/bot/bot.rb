3e9e840320095da07ac1da4cb4c8d2e5e54f7abd
EC2 Default User <ec2-user@ip-172-31-17-18.us-east-2.compute.internal>
2018-06-28 21:38:03 +0000

    3e commit

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,13 +5,31 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
+    message.mark_seen
     if message.text.include? "Bonjour"
        message.reply(text:"Bonjour")
     elsif 
        message.text.include? "ca va"
        message.reply(text: "oui et vous?")
+    elsif
+        message.reply(
+        attachment: {
+        type: 'image',
+        payload: {
+        url: 'http://sky.net/visual-aids-for-stupid-organisms/pig.jpg'
+            
+        }
+            
+        }
+)
+    elsif 
+       message.text.include? "article"
+       message.reply(text: "Quel type d'article?")
+    elsif 
+       message.text.include? "ticket" or "Ticket"
+       message.reply(text: "Type incident ou demande?")
     else
-       message.reply(text: "Avez vous une question précise?")
+       message.reply(text: "Avez vous une question précise?") 
     end
 
 end
