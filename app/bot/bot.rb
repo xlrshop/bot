ebd2752baeb27c7801ee92b3213fb5c76f5c69b8
EC2 Default User <ec2-user@ip-172-31-17-18.us-east-2.compute.internal>
2018-06-28 21:03:16 +0000

    2e commit

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,8 +2,16 @@ require "facebook/messenger"
 
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV ["ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+    if message.text.include? "Bonjour"
+       message.reply(text:"Bonjour")
+    elsif 
+       message.text.include? "ca va"
+       message.reply(text: "oui et vous?")
+    else
+       message.reply(text: "Avez vous une question précise?")
+    end
+
 end
